ae63bbb13389a927247a1eaedef552105eccf81b
chi <nguyenlinhchi09@gmail.com>
2018-06-12 19:47:34 +0200

    plot how much it gives

--- auto.rkt
+++ auto.rkt
@@ -403,3 +403,19 @@
            (interact-g i BENCHMARKS)))))
   
   
+;; kit
+(define (match-classics au)
+  (match-define (cons a1 a2) (interact-r au au))
+  (define itself
+    (/ (+ a1 a2) 2))
+  (define (with-au2 au2)
+    (match-define (cons a1 a2) (interact-r au au2))
+    a1)
+(define (to-au2 au2)
+(match-define (cons a1 a2) (interact-r au au2))
+a2)
+  (define thigh (to-au2 (H)))
+  (define tmedium (to-au2 (M)))
+  (define wlow (with-au2 (L)))
+  (define taccom (to-au2 (A)))
+  (list itself wlow tmedium thigh taccom)) 
